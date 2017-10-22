0f92ed49d02fa27b4448f08e173ddfed9b1656af
Anne L'Hôte <anne.lhote@gmail.com>
2017-10-23 00:28:43 +0200

    Clean code and make it works

--- script.py
+++ script.py
@@ -22,15 +22,14 @@ import requests
 log_file = 'RapRapesYourEars.log'
 log_level = logging.DEBUG
 base_url = 'http://api.genius.com'
-
+artist_name = "IAM"
 
 #
 # Functions
 #
 def get_lyrics_from_song_id(song_id) :
-	logging.info('Collect lyrics from song : ' + song_id)
-	song_url = base_url + '/songs/' + song_id
-	headers = {'Authorization': 'Bearer ' + conf['bearer']}
+	logging.debug('Collect lyrics from song : ' + str(song_id))
+	song_url = base_url + '/songs/' + str(song_id)
 	json = requests.get(song_url, headers = headers).json()
 	# Check if this song already exists in database
 	if db.song.find({'_id' : song_id}).count() == 0 :
@@ -54,24 +53,39 @@ def get_lyrics_from_song_id(song_id) :
 		# Create lyrics as a JSON Object
 		lyric = {
 			'_id': song_id,
-			'text': html.find('lyrics').getText(),
+			'text': html.find('div', class_='lyrics').get_text(),
 			'creation_date': datetime.datetime.utcnow()
 		}
 		# Save lyrics in database
 		db.lyrics.insert_one(lyric)
 
+def get_songs_from_artist_id(artist_id) :
+	artist_url = base_url + '/artists/' + str(artist_id) + '/songs'
+	params = {"per_page": 50, "page": 1}
+	# Loop over pages
+	while True:
+		response = requests.get(artist_url, params=params, headers=headers).json()
+		songs = response["response"]["songs"]
+		logging.debug('Retrieving page ' + str(params['page']) + ' of songs list.')
+		for song in songs:
+			get_lyrics_from_song_id(song["id"])
+		if len(songs) < 50:
+			break
+		else :
+			params["page"] = response["response"]["next_page"]
 
-def get_songs_from_album_url(url) :
-	logging.info('Collect songs from album : ' + url)
-	page = requests.get(url)
-	html = BeautifulSoup(page.text, 'html.parser')
-	children = html.find_all('ul', class_ = 'song_list')[0].children
-	for child in children :
-		if child.name != None :
-			get_lyrics_from_song_id(child['data-id'])
+def get_artist_id_from_artist_name(artist_name) :
+	search_url = base_url + '/search'
+	params = {'q': artist_name}
+	response = requests.get(search_url, params=params, headers=headers).json()
+	artist_id = 0
+	for hit in response["response"]["hits"] :
+		if hit["result"]["primary_artist"]["name"].lower() == artist_name.lower() :
+			artist_id =  hit["result"]["primary_artist"]["id"]
+	return artist_id
 
 def main() :
-	global conf, db
+	global conf, db, headers
 	# Init logs
 	logging.basicConfig(filename = log_file, filemode = 'a+', format = '%(asctime)s  |  %(levelname)s  |  %(message)s', datefmt = '%m/%d/%Y %I:%M:%S %p', level = log_level)
 	logging.info('Start')
@@ -87,8 +101,15 @@ def main() :
 	# Connect to Mongo DB
 	client = pymongo.MongoClient()
 	db = client.RapRapesYourEars
-	# IAM - L'école du micro d'argent
-	get_songs_from_album_url('http://genius.com/albums/Iam/L-ecole-du-micro-d-argent')
+	logging.debug('Connected to the Mongo database.')
+	headers = {'Authorization': 'Bearer ' + conf['bearer']}
+	artist_id = get_artist_id_from_artist_name(artist_name)
+	logging.debug('The artist id for ' + artist_name + ' is : ' + str(artist_id) + '.')
+	if artist_id == 0 :
+		logging.error('This artist name doesn\'t exist in Genius.com')
+		exit()
+	else :
+		get_songs_from_artist_id(artist_id)
 	logging.info('End')
 
 
